a7de3efd08e9484dba74fe0eed6bf2b1456ad2e4
03Kristian <cristian.jacanamijoy@misena.edu.co>
2022-05-31 18:17:10 -0500

    v3

---
+++ Jumbo_368/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<div class="col-lg-6col-md-4col-sm-2">
+    <select class="form-select"aria-label="Default select example">
+        <option selected>Nivel académico</option>
+        <option value="1">Bachiller académicoy/ocomercial</option>
+       <option value="2"></option>
+        <option value="3">Three</option>
+     </select>
+              I
+  </div>
+
+  
